f063971213ace0e69d5a60077f5a104575c4b1b0
jotek321 <83513501+jotek321@users.noreply.github.com>
2022-04-03 21:06:05 +0200

    Update Kalkulator.py
    now it work continuous

--- Kalkulator.py
+++ Kalkulator.py
@@ -1,24 +1,20 @@
-import time
+while True:
+    print('To jest kalkulator\nUżycie liczby zcrashuje calculatorn\n\n')
+    num1 = float(input('liczba x równa się: \n'))
+    num2 = float(input('liczba y równa się: \n'))
+    fun = int(input('funkcje \n'
+                    '1 to dodawanie,\n'
+                    '2 to odejmowanie,\n'
+                    '3 to mnożenie,\n'
+                    '4 to dzielenie\n'))
 
-print('To jest kalkulator')
-num1 = float(input('liczba x równa się: \n'))
-num2 = float(input('liczba y równa się: \n'))
-
-fun = int(input('funkcje \n'
-                '1 to dodawanie,\n'
-                '2 to odejmowanie,\n'
-                '3 to mnożenie,\n'
-                '4 to dzielenie\n'))
-
-if fun == 1:
-    print(num1 + num2)
-elif fun == 2:
-    print(num1 - num2)
-elif fun == 3:
-    print(num1 * num2)
-elif fun == 4:
-    print(num1 / num2)
-else:
-    print('ERROR')
-
-time.sleep(10)
+    if fun == 1:
+        print(num1 + num2)
+    elif fun == 2:
+        print(num1 - num2)
+    elif fun == 3:
+        print(num1 * num2)
+    elif fun == 4:
+        print(num1 / num2)
+    else:
+        print('ERROR')
