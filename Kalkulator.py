f30f2ad8ab4de6e3d7f419742e1ad32c2551862e
jotek321 <83513501+jotek321@users.noreply.github.com>
2022-04-17 21:35:16 +0200

    Update.
    It's now a multipurpose tool. I will now add more features in the future.
    Fixed issue with inputing an letter.

--- Kalkulator.py
+++ Kalkulator.py
@@ -1,20 +1,45 @@
-while True:
-    print('It is a calculator\nIf you use a character i will crash\n\n')
-    num1 = float(input('liczba x równa się: \n'))
-    num2 = float(input('liczba y równa się: \n'))
-    fun = int(input('funkcje \n'
-                    '1 to dodawanie,\n'
-                    '2 to odejmowanie,\n'
-                    '3 to mnożenie,\n'
-                    '4 to dzielenie\n'))
+def calculator():
+    try: 
+        print('Using a letter will resolt in crash\n')
+        num1 = float(input('x is equal to: \n'))
+        num2 = float(input('y is equal to: \n'))
+        fun =  (input('functions \n'
+                        '1 is addition,\n'
+                        '2 is subtraction,\n'
+                        '3 is multiplication,\n'
+                        '4 is division.\n'))
 
-    if fun == 1:
-        print(num1 + num2)
-    elif fun == 2:
-        print(num1 - num2)
-    elif fun == 3:
-        print(num1 * num2)
-    elif fun == 4:
-        print(num1 / num2)
+        if fun == '1':
+            print(num1 + num2)
+        elif fun == '2':
+            print(num1 - num2)
+        elif fun == '3':
+            print(num1 * num2)
+        elif fun == '4':
+            print(num1 / num2)
+        else:
+            print('error')
+
+    except ZeroDivisionError as exeption_name:
+        print(exeption_name)
+        print("You can't divide by 0.")
+    except ValueError as exeption_name:
+        print(exeption_name)
+        print('Enter only numbers.')
+    except Exception as exeption_name:
+        print(exeption_name)
+        print('Some thing went wrong.')
+
+while True:
+    print('It is multipurpose tool')
+    option = input('To open: \n calculator enter 1 \n')
+
+    if option == '1':
+        calculator()
+    elif option > '1':
+        print('error wrong number')
+    elif option < '1':
+        print('error wrong number')
     else:
-        print('ERROR')
+         pass
+
